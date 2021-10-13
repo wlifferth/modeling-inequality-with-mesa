073296266d39c0cd183f641dc1b4f7e7f80459e0
wlifferth <will.lifferth@gmail.com>
2021-10-12 20:18:33 -0700

    Make agents give money to a neighbor.

--- MoneyModel.py
+++ MoneyModel.py
@@ -17,7 +17,9 @@ class MoneyAgent(Agent):
         self.wealth = 1
 
     def step(self):
-      self.move()
+        self.move()
+        if self.wealth > 0:
+            self.give_money()
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
@@ -26,6 +28,13 @@ class MoneyAgent(Agent):
           include_center=True)
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
+
+    def give_money(self):
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmates) > 1:
+            other = self.random.choice(cellmates)
+            other.wealth += 1
+            self.wealth -= 1
 
 
 class MoneyModel(Model):
